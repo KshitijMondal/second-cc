5bd5e9087569410720bc8ad7d5b1899a1b789c1d
KshitijMondal <kshitij05.mondal@gmail.com>
2026-04-27 07:17:08 +0530

    Add simple calculator program
    Implemented a simple calculator that performs basic arithmetic operations based on user input.

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print("Hello, this is a simple calculator program")
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+sign = input("Enter operator(+, -, *, /, %): ")
+o = sign.strip()
+
+if o == "+":
+  print(a,"+","b","="(a+b))
+elif o == "-":
+  print(a,"-","b","="(a-b))
+elif o == "*":
+  print(a,"*","b","="(a*b))
+elif o == "/":
+  print(a,"/","b","="(a/b))
+elif o == "%":
+  print(a,"%","b","="(a%b))
+else:
+  print("Please enter valid operator!")
